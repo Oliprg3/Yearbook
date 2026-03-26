4fee0b3ba6c52baf6eb9fc83b6316725e06f9404
Oliprg3 <136181155+Oliprg3@users.noreply.github.com>
2026-03-26 23:00:53 +0300

    Refactor Post model to remove createdAt field

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -11,13 +11,10 @@ const PostSchema = new mongoose.Schema({
     },
     author: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
+        ref: 'User'
     }
+}, {
+    timestamps: true
 });
 
 module.exports = mongoose.model('Post', PostSchema);
