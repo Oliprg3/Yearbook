241c8f848dfd52f408387836c5990d026ab6bc10
Oliprg3 <136181155+Oliprg3@users.noreply.github.com>
2026-03-27 07:20:56 +0300

    Refactor PostSchema definition for cleaner syntax

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -1,20 +1,9 @@
 const mongoose = require('mongoose');
 
 const PostSchema = new mongoose.Schema({
-    content: {
-        type: String,
-        required: true
-    },
-    image: {
-        type: String,
-        default: null
-    },
-    author: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    }
-}, {
-    timestamps: true
-});
+    content: { type: String, required: true },
+    image: { type: String, default: null },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+}, { timestamps: true });
 
 module.exports = mongoose.model('Post', PostSchema);
